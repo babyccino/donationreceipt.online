7916de5343dbe1a82a6c5f0e649e4afce175fcba
Gus <gus.ryan163@gmail.com>
2024-03-09 01:51:43 +0000

    build: add more commands

--- apps/lambdas/container-cmds.sh
+++ apps/lambdas/container-cmds.sh
@@ -1,26 +1,29 @@
 # build
 docker build --build-arg TURBO_TEAM=babyccino,TURBO_TOKEN=RzOeTr289p6T6ksMCFBwlRC5 --platform linux/arm64 \
-    -f ./apps/lambdas/Dockerfile -t AWS_ID.dkr.ecr.us-east-2.amazonaws.com/donationreceipt-online:latest .
-
-# test build
-docker build --build-arg TURBO_TEAM=babyccino,TURBO_TOKEN=RzOeTr289p6T6ksMCFBwlRC5 --platform linux/arm64 \
-    -f ./Dockerfile -t dono-test .
+    -f ./apps/lambdas/Dockerfile -t $ECR_REPO/$TAG
 
 # run
 docker run -m 1024m --rm --env-file ./apps/lambdas/.env --platform linux/arm64 -p 9000:8080 -it \
-    AWS_ID.dkr.ecr.us-east-2.amazonaws.com/donationreceipt-online:latest
+    $ECR_REPO/$TAG
 
 # test run
 docker run -m 1024m --rm --env-file ./apps/lambdas/.env --platform linux/arm64 -p 9000:8080 -it \
     dono-test
 
-
 # login
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin \
-    AWS_ID.dkr.ecr.us-east-2.amazonaws.com
+    $ECR_REPO
 
 # tag
-docker tag dono-lambda:0.1 AWS_ID.dkr.ecr.us-east-2.amazonaws.com/donationreceipt-online:latest
+docker tag dono-lambda:0.1 $ECR_REPO/$TAG
 
 # push
-docker push AWS_ID.dkr.ecr.us-east-2.amazonaws.com/donationreceipt-online:latest
+docker push $ECR_REPO/$TAG
+
+# build & push
+docker build --build-arg TURBO_TEAM=babyccino,TURBO_TOKEN=RzOeTr289p6T6ksMCFBwlRC5 --platform linux/arm64 \
+    -f ./apps/lambdas/Dockerfile -t $ECR_REPO/$TAG . \
+    && docker push $ECR_REPO/$TAG
+
+# deploy
+aws lambda update-function-code --function-name dono-lambda --image-uri $ECR_REPO/$TAG

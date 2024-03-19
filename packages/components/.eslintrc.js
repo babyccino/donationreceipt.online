4ae7c4abe4fc7a9b3be7a44f012b917d2a148d01
Gus <gus.ryan163@gmail.com>
2024-03-19 00:44:54 +0000

    fix: fix eslint bug in /components

--- packages/components/.eslintrc.js
+++ packages/components/.eslintrc.js
@@ -1,6 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["@repo/eslint-config/react.js"],
+  // extends: ["@repo/eslint-config/react.js"],
+  extends: ["@repo/eslint-config/index.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,

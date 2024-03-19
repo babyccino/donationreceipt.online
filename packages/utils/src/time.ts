75d91499c9d14883a411986d52cbc6dfcb2ff627
Gus <gus.ryan163@gmail.com>
2024-03-19 12:23:19 +0000

    auth: increase session time to 3 days

---
+++ packages/utils/src/time.ts
@@ -0,0 +1,21 @@
+export namespace toMs {
+  export const millisecond = 1
+  export const second = 1000
+  export const minute = 60 * second
+  export const hour = 60 * minute
+  export const day = 24 * hour
+  export const week = 7 * day
+  export const month = 30 * day
+  export const year = 365 * day
+}
+
+export namespace toSeconds {
+  export const ms = 1 / 1000
+  export const second = 1
+  export const minute = 60 * second
+  export const hour = 60 * minute
+  export const day = 24 * hour
+  export const week = 7 * day
+  export const month = 30 * day
+  export const year = 365 * day
+}
